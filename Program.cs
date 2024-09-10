a1469acbaf2bdc81786da56590aa0ae6b823aa89
Maikin <custodiomaikin@gmail.com>
2024-09-10 12:10:40 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+
+namespace REGISTRO_DE_USUARIO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            UserRegistry registry = UserRegistry.Instance;
+
+            Console.WriteLine("Este programa gestionara usuarios \n");
+
+            bool activo = true;
+            while (activo == true)
+            {
+                Console.WriteLine("\nSelecciona una opcion: \n");
+                Console.WriteLine("1.Añadir usuario.");
+                Console.WriteLine("2.Eliminar usuario.");
+                Console.WriteLine("3.Mostrar cantidad de usuarios.");
+                Console.WriteLine("4.Mostrar todos los usuarios.");
+                Console.WriteLine("5.Salir del programa. \n");
+                int opcion = Convert.ToInt32(Console.ReadLine());
+                var validacion = false;
+
+                while (!validacion)
+                {
+                    string Nombre;
+                    switch (opcion)
+                    {
+                        case 1:
+                            Console.WriteLine("\nIntroduzca el nombre del usuario: ");
+                            Nombre = Console.ReadLine();
+                            registry.AddUser(Nombre); 
+                            validacion = true;
+                            activo = true;
+                            break;
+
+                        case 2:
+                            Console.WriteLine("\nIntroduzca el nombre del usuario que desea eliminar. \nEsta es una lista de los usuarios guardados:  ");
+                            registry.ListaUsuarios();
+                            Nombre = Console.ReadLine();
+                            registry.DelUser(Nombre);
+                            validacion = true;
+                            activo = true;
+                            break;
+
+                        case 3:
+                            Console.WriteLine("\nEsta es la cantidad de usuarios actualmente: ");
+                            registry.TotalUsuarios();
+                            validacion = true;
+                            activo = true;
+                            break;
+
+                        case 4:
+                            Console.WriteLine("\nEsta es una lista de todos lo nombres: ");
+                            registry.ListaUsuarios();
+                            validacion = true;
+                            activo = true;
+                            break;
+
+                        case 5:
+                            Console.WriteLine("El programa ha finalizado. \n");
+                            Console.WriteLine("El programa se debe acabar \n");
+                            validacion = true;
+                            activo = false;
+                            break;
+
+                        default:
+                            Console.WriteLine("La tecla presionada no estaba en la lista de opciones, se deberia pedir la opcion nuevamente \n");
+                            Console.WriteLine("Elige de nuevo: \n");
+                            opcion = Convert.ToInt32(Console.ReadLine());
+                            activo = true;
+                            break;
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ UserRegistry.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace REGISTRO_DE_USUARIO
+{
+    internal class UserRegistry
+    {
+        private List<string> Usuarios;
+        private static UserRegistry instance = null;
+
+        private UserRegistry()
+        {
+            Usuarios = new List<string>();
+        }
+
+
+        public static UserRegistry Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new UserRegistry();
+
+                return instance;
+            }
+        }
+        
+        public void AddUser(string Nombre)
+        {
+             Usuarios.Add(Nombre);
+             Repeticion(Nombre);
+        }
+
+        public void DelUser(string Nombre)
+        {
+            Usuarios.Remove(Nombre);
+            Console.WriteLine($"El usuario {Nombre} ha sido eliminado \n");
+        }
+
+        public void TotalUsuarios()
+        {
+            Console.WriteLine($"{Usuarios.Count()}"); 
+        }
+
+        public void Repeticion(string Nombre)
+        {
+            for (int i = 0; i < Usuarios.Count; i++)
+            {
+                if (Usuarios[i].ToLower() == Nombre.ToLower()) 
+                {           
+                    int max = Usuarios.Count(); 
+                    if (max > i + 1)
+                    {
+                        if (Usuarios[i].ToLower() == Nombre.ToLower())
+                        {
+                            Console.WriteLine("Este nombre es similar o ya existe en el listado,¿desea agregarlo de todos modos?\n Presione\n 1.Si \n 2.No");
+                            int opcion = Convert.ToInt32(Console.ReadLine());
+                            if (opcion == 1)
+                            {
+                                Console.WriteLine($"Cambio Guardado\n");
+                            }
+                            else
+                            {
+                                Usuarios.Remove(Nombre);
+                                Console.WriteLine($"El nombre: {Nombre} ha sido eliminado del listado");
+                            }
+                        }   
+                    }       
+                }
+            }
+        }
+
+        public void ListaUsuarios()
+        {
+            if (Usuarios.Count <= 0)
+            {
+                Console.WriteLine("No hay usuarios en el sistema");
+            }
+            else
+            {
+                foreach (var usuario in Usuarios)
+                {
+                    Console.WriteLine(usuario);
+                }
+            }
+        }
+    }
+}
